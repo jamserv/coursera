15c542d48ef4df41bd8a397b6b4deb2143cc9aa0
jamsev@gmail.com <jamserv@gmail.com>
2017-06-19 20:25:58 -0500

    r-programing

--- data-science/files/pollutantmean.R
+++ data-science/files/pollutantmean.R
@@ -1,4 +1,8 @@
 pollutantmean <- function(directory, pollutant, id = 1:323) {
+        ##Directory : location of load *.csv file.
         file <- read.csv(directory)
+        
+        ##pollutant : is character vector indicate type of pollutant.
+        
         
 }
