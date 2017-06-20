997e2cf50f8dad2d65295fd11e44c8871cba79dc
jamsev@gmail.com <jamserv@gmail.com>
2017-06-19 20:44:39 -0500

    r-programing

--- data-science/files/pollutantmean.R
+++ data-science/files/pollutantmean.R
@@ -1,8 +1,14 @@
 pollutantmean <- function(directory, pollutant, id = 1:323) {
         ##Directory : location of load *.csv file.
-        file <- read.csv(directory)
-        
+        loadData <- read.csv(directory)
         ##pollutant : is character vector indicate type of pollutant.
-        
-        
+        result <- 0
+        if(pollutant == 'nitrate') {
+                result <- mean(loadData$nitrate[!is.na(loadData$nitrate)])
+        } else if(pollutant == 'sulfate') {
+                result <- mean(loadData$sulfate[!is.na(loadData$sulfate)])
+        } else {
+                warning('option not available. Try again')
+        }
+        result
 }
